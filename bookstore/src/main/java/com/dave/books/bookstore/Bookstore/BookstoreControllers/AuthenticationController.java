7751cb87123609f2d8d4942ab8b0e7314b3e0610
Peter David <Princewill016@gmail.com>
2024-04-04 22:59:22 +0100

    implementing security

--- bookstore/src/main/java/com/dave/books/bookstore/Bookstore/BookstoreControllers/AuthenticationController.java
+++ bookstore/src/main/java/com/dave/books/bookstore/Bookstore/BookstoreControllers/AuthenticationController.java
@@ -1,5 +1,20 @@
 package com.dave.books.bookstore.Bookstore.BookstoreControllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping(path = "api/v2/authenticate")
 public class AuthenticationController {
+
+@PostMapping("/authenticate")
+public String postMethodName(@RequestBody String entity) {
+    //TODO: process POST request
+    
+    return entity;
+}
 
 }
---
+++ bookstore/src/main/java/com/dave/books/bookstore/DataTransferObject/AuthenticationRequestDto.java
@@ -0,0 +1,5 @@
+package com.dave.books.bookstore.DataTransferObject;
+
+public class AuthenticationRequestDto {
+
+}
